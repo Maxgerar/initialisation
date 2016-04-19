253f796430440b5fb66bb9733f8104e386640c57
maxgerar <maxgerar@ulb.ac.be>
2016-04-19 12:17:38 -0400

    Initial Commit

---
+++ Initialisation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Initialisation
+//
+//  Created by Maxime Gérard on 19/04/16.
+//  Copyright © 2016 Maxime Gérard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
